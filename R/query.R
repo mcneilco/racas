27487e7e33a52636cb719a2534eee9b24c842fac
Brian Bolt <brian@mcneilco.com>
2013-04-16 09:18:36 -0700

    better query support

--- R/query.R
+++ R/query.R
@@ -1,6 +1,11 @@
 query <- function(qu) {
-  conn <- dbConnect(eval(parse(text = applicationSettings$db_driver)), user= applicationSettings$db_user, password=applicationSettings$db_password, dbname=applicationSettings$db_name, host=applicationSettings$db_host, port=applicationSettings$db_port)
-  result <- dbGetQuery(conn,qu)
-  dbDisconnect(conn)
+  driver <- eval(parse(text = applicationSettings$db_driver))
+  conn <- switch(class(driver),
+         "OraDriver" = dbConnect(driver, dbname=paste0(applicationSettings$db_host,":",applicationSettings$db_port,"/",applicationSettings$db_name), user=applicationSettings$db_user, pass=applicationSettings$db_password),
+          "PostgreSQLDriver" = DBI::dbConnect(driver , user= applicationSettings$db_user, password=applicationSettings$db_password, dbname=applicationSettings$db_name, host=applicationSettings$db_host, port=applicationSettings$db_port)
+          "MySQLDriver" = DBI::dbConnect(driver , user= applicationSettings$db_user, password=applicationSettings$db_password, dbname=applicationSettings$db_name, host=applicationSettings$db_host, port=applicationSettings$db_port)
+  )
+  result <- DBI::dbGetQuery(conn,qu)
+  DBI::dbDisconnect(conn)
   return(result)
 }
