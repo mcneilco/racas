9b831fad1072dc88378cef7123e46add65dd4984
Brian Bolt <brian@mcneilco.com>
2013-04-19 10:38:03 -0700

    query issues

--- R/query.R
+++ R/query.R
@@ -15,8 +15,22 @@ query <- function(qu) {
         "PostgreSQLDriver" = DBI::dbConnect(driver , user= applicationSettings$db_user, password=applicationSettings$db_password, dbname=applicationSettings$db_name, host=applicationSettings$db_host, port=applicationSettings$db_port),
         "MySQLDriver" = DBI::dbConnect(driver , user= applicationSettings$db_user, password=applicationSettings$db_password, dbname=applicationSettings$db_name, host=applicationSettings$db_host, port=applicationSettings$db_port),
         "JDBCDriver" = DBI::dbConnect(driver, paste0(getDBString(applicationSettings$db_driver),applicationSettings$db_host,":",applicationSettings$db_port,":",applicationSettings$db_name), user=applicationSettings$db_user, pass=applicationSettings$db_password),
+        class(driver)
   )
-  result <- DBI::dbGetQuery(conn,qu)
+  tryCatch({
+    result <- DBI::dbGetQuery(conn,qu)
+  },
+  error = function(ex) {
+    if(class(conn)=="character") {
+      if(conn==class(driver)) {
+        stop("Unrecognized driver class '",class(driver),"', applicationSettings$db_driver '",parse(text = applicationSettings$db_driver), "' evals to class ", class(driver),", must evaluate to a known driver class\n see ?racas:::query")
+      }
+    } else{
+      print(ex)
+      return(NULL)
+    }
+  }, finally = {
+  })
   DBI::dbDisconnect(conn)
   return(result)
 }
