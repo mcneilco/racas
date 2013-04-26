54a486fa2c9a43e17ffc3f48625e67a07b1af5dc
Brian Bolt <brian@mcneilco.com>
2013-04-25 19:28:38 -0700

    added "globalConnect" option that will assume there is a global database "conn" variable that it can query with.  If the conn object does not exist, the function will create it and use it for the connection and leave it open in the global name space. Probably some issues with this but I need to get this working in Rapache to speed up curve rendering.

--- R/query.R
+++ R/query.R
@@ -1,36 +1,58 @@
-query <- function(qu) {
-  getDBString <- function(driverString) {
-    supportedDBs <- c("oracle", "postgres", "mysql")
-    db <- supportedDBs[unlist(lapply(supportedDBs, grep, x = driverString))[1]]
-    dbString <- switch(db,
-           "oracle" = "jdbc:oracle:thin:@",
-           "postgres" = "jdbc:postgresql://",
-           "mysql" = "jdbc:mysql://"
-           )
-    return(dbString)
+errorHandler <- function(ex) {
+  if(class(conn)=="character") {
+    if(conn==class(driver)) {
+      print("Unrecognized driver class '",class(driver),"', racas::applicationSettings$db_driver '",parse(text = racas::applicationSettings$db_driver), "' evals to class ", class(driver),", must evaluate to a known driver class\n see ?racas:::query")
+      return(NULL)
+    }
+  } else{
+    print(ex)
+    return(NULL)
   }
-  driver <- eval(parse(text = racas::applicationSettings$db_driver))
-  conn <- switch(class(driver),
-        "OraDriver" = DBI::dbConnect(driver, dbname=paste0(racas::applicationSettings$db_host,":",racas::applicationSettings$db_port,"/",racas::applicationSettings$db_name), user=racas::applicationSettings$db_user, pass=racas::applicationSettings$db_password),
-        "PostgreSQLDriver" = DBI::dbConnect(driver , user= racas::applicationSettings$db_user, password=racas::applicationSettings$db_password, dbname=racas::applicationSettings$db_name, host=racas::applicationSettings$db_host, port=racas::applicationSettings$db_port),
-        "MySQLDriver" = DBI::dbConnect(driver , user= racas::applicationSettings$db_user, password=racas::applicationSettings$db_password, dbname=racas::applicationSettings$db_name, host=racas::applicationSettings$db_host, port=racas::applicationSettings$db_port),
-        "JDBCDriver" = DBI::dbConnect(driver, paste0(getDBString(racas::applicationSettings$db_driver),racas::applicationSettings$db_host,":",racas::applicationSettings$db_port,":",racas::applicationSettings$db_name), user=racas::applicationSettings$db_user, pass=racas::applicationSettings$db_password),
-        class(driver)
-  )
+}
+query <- function(qu, globalConnect=FALSE) {
+  if(!globalConnect) {
+    conn <- getDabaseConnection()
+  }
   tryCatch({
     result <- DBI::dbGetQuery(conn,qu)
   },
   error = function(ex) {
-    if(class(conn)=="character") {
-      if(conn==class(driver)) {
-        stop("Unrecognized driver class '",class(driver),"', racas::applicationSettings$db_driver '",parse(text = racas::applicationSettings$db_driver), "' evals to class ", class(driver),", must evaluate to a known driver class\n see ?racas:::query")
-      }
-    } else{
-      print(ex)
-      return(NULL)
+    if(globalConnect) {
+      conn <<- getDabaseConnection()
+      tryCatch({
+        result <- DBI::dbGetQuery(conn,qu)
+      },
+      error = function(ex) {
+        errorHandler(ex, conn, driver)
+      })
+    } else {
+      errorHandler(ex, conn, driver)
     }
   }, finally = {
   })
-  DBI::dbDisconnect(conn)
+  if(!globalConnect) {
+    DBI::dbDisconnect(conn)
+  }
   return(result)
+}
+getDabaseConnection <- function() {
+  getDBString <- function(driverString) {
+    supportedDBs <- c("oracle", "postgres", "mysql")
+    db <- supportedDBs[unlist(lapply(supportedDBs, grep, x = driverString))[1]]
+    dbString <- switch(db,
+                       "oracle" = "jdbc:oracle:thin:@",
+                       "postgres" = "jdbc:postgresql://",
+                       "mysql" = "jdbc:mysql://"
+    )
+    return(dbString)
+  }
+  driver <- eval(parse(text = racas::applicationSettings$db_driver))
+  conn <- switch(class(driver),
+                 "OraDriver" = DBI::dbConnect(driver, dbname=paste0(racas::applicationSettings$db_host,":",racas::applicationSettings$db_port,"/",racas::applicationSettings$db_name), user=racas::applicationSettings$db_user, pass=racas::applicationSettings$db_password),
+                 "PostgreSQLDriver" = DBI::dbConnect(driver , user= racas::applicationSettings$db_user, password=racas::applicationSettings$db_password, dbname=racas::applicationSettings$db_name, host=racas::applicationSettings$db_host, port=racas::applicationSettings$db_port),
+                 "MySQLDriver" = DBI::dbConnect(driver , user= racas::applicationSettings$db_user, password=racas::applicationSettings$db_password, dbname=racas::applicationSettings$db_name, host=racas::applicationSettings$db_host, port=racas::applicationSettings$db_port),
+                 "JDBCDriver" = DBI::dbConnect(driver, paste0(getDBString(racas::applicationSettings$db_driver),racas::applicationSettings$db_host,":",racas::applicationSettings$db_port,":",racas::applicationSettings$db_name), user=racas::applicationSettings$db_user, pass=racas::applicationSettings$db_password),
+                 class(driver)
+  )
+  return(conn)
 }
