fab9aa0d204bd80e6947d2f21eafd9e590f71da8
Brian Bolt <brian@mcneilco.com>
2013-10-04 09:59:23 -0700

    adding the option to read from ACAS_CONFIG as well as ACAS_HOME.
    ACAS_CONFIG environment variable should point directly at the configuration node.js
    
    ACAS_CONFIG will take priority over ACAS_HOME

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,21 +1,30 @@
 .onLoad <- function(libname, pkgname) {
+  appConfName <- paste0(applicationSettings$appName,"_CONFIG")
   appHomeName <- paste0(applicationSettings$appName,"_HOME")
+  appConf <- Sys.getenv(appConfName)
   appHome <- Sys.getenv(appHomeName)
-  if(appHome == "") {
-    warning(paste0(appHomeName," environment variable not set, not reading application settings.\n\n",
-                  "You can set this prior to loading this package by running:\n",
-                   "\tSys.setenv(",appHomeName," = \"/path/to/SeuratAddOns\")\n\n",
-                   "Or, you can set this in your system environment before loading R"),call.=FALSE)
+  if(appHome == ""  && appConf == "") {
+    warning(paste0(appHomeName," and ",appConfName," environment variables not set, not reading application settings.\n\n",
+                   "You can set this prior to loading this package by running:\n",
+                   "\tSys.setenv(",appHomeName," = \"/path/to/",applicationSettings$appName,"\")\n",
+                   "Or\n",
+                   "\tSys.setenv(",appConfName," = \"/path/to/",applicationSettings$appName,"_config_file\")\n\n",
+                   "Or, you can set either of these in your system environment before loading R. The config file location will take priority over home"),call.=FALSE)
   } else {
     tryCatch({
-        configFileLocation <- paste0(appHome,"/public/src/conf/configurationNode.js")
+      if(appHome!="") {
+        configFileLocation <- file.path(appHome,"/public/src/conf/configurationNode.js")
         output <- readConfigFile(configFileLocation)
-      },error = function(ex) {
-        #error <- capture.output(print(ex))
-        warning(paste0("Could not load configuration file at:\n\t",
-                       configFileLocation,"\n\n",
-                       "This was the captured error:\n",
-                       "\t",ex),call.=FALSE)
-      })
+      } else {
+        configFileLocation <- file.path(appConf)
+        output <- readConfigFile(configFileLocation)
+      }
+    },error = function(ex) {
+      #error <- capture.output(print(ex))
+      warning(paste0("Could not load configuration file at:\n\t",
+                     configFileLocation,"\n\n",
+                     "This was the captured error:\n",
+                     "\t",ex),call.=FALSE)
+    })
   }
 }
