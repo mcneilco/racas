05aaa0d83ddff50e73001c9094189b26aacc2a09
Brian Bolt <brian@mcneilco.com>
2013-10-23 15:50:44 -0700

    new logging and messaging stuff

---
+++ R/logger.R
@@ -0,0 +1,117 @@
+#'Logger class (subclass of Logger in logging package)
+#'
+#'Logger class (subclass of Logger in logging package)
+#'
+#'
+#'
+Logger <- setRefClass( 
+  Class="racasLogger", 
+  fields=list( 
+    envir = "environment"
+  ), 
+  contains = "Logger", 
+  methods=list( 
+    
+  ) 
+)
+
+#'Creates a new logger
+#'
+#'Creates a new logger
+#'
+#'@param logName  Name for the logger output line within the log file (default: "com.default.logger)
+#'@param logFileName name of the log file to write to (default: "output.log")
+#'@param logDir name directory to write to (default: racas::applicationSettings$logDir)
+#'@param logLevel the level at which to write logs (default: racas::applicationSettings$logLevel, see names(logging::loglevels))
+#'@param envir an environment to attach to the object
+#'@return object of class logger
+#'
+#'@examples
+#' #Creating a logger with logLevel = "WARN"
+#' myLogger <- createLogger(logName = "com.mycompany.newprocessor", 
+#'                          logFileName = "newprocessor.log", 
+#'                          logLevel = "WARN")
+#' 
+#' #Will write
+#' myLogger$error("a warn statement")
+#' myLogger$warn("a warn statement")
+#' 
+#' #Won't write items that are below "WARN" in names(logging::loglevels)
+#' myLogger$debug("a debug statement")
+#' myLogger$info("a warn statement")
+#' 
+#' #Creating a logger with logLevel = "DEBUG"
+#' myLogger <- createLogger(logName = "com.mycompany.newprocessor", 
+#'                          logFileName = "newprocessor.log", 
+#'                          logLevel = "DEBUG")
+#' 
+#' #Now it will log debug and info statements
+#' myLogger$debug("a debug statement")
+#' myLogger$info("a warn statement")
+#' 
+createLogger <- function(logName = "com.default.logger", logFileName = "output.log", logDir = racas::applicationSettings$logDir, logLevel = racas::applicationSettings$logLevel, envir = environment(), ...) {
+  if(is.null(logLevel)) logLevel <- "INFO"
+  basicConfig(level = logLevel)
+  if(is.na(logDir)) {
+    logDir <-  getwd()
+  }
+  logPath <- paste0(logDir,"/",logFileName)
+  getLogger(logName)$addHandler(writeToFile, file=logPath, level = logLevel)
+  logger <- getLogger(logName)
+  setLevel(logLevel, logger)
+  return(logger)
+}
+
+bubbleLogger <- Logger$new()
+
+#'Creates a new logger object or returns the "bubbleLogger"
+#'
+#'Optionally creates a new logger or returns a logger stored in the racas namespace.
+#'The default call 'logger()' will compare the field racas::bubbleLogger$envir with the current list of frames in the call stack.
+#'If the environment in the bubbleLogger matches one of the environments in the current call stack, then the bubbleLogger is returned.
+#'
+#'@param bubble  Name for the logger output line within the log file (default: "com.default.logger)
+#'@param envir name of the log file to write to (default: "output.log")
+#'@param ... further arguments to be passed to \code{\link{createLogger}}
+#'@return object of class \code{\link{Logger}}
+#'
+#'@examples
+#' #From a fres
+#' 
+#' #Will write
+#' myLogger$error("a warn statement")
+#' myLogger$warn("a warn statement")
+#' 
+#' #Won't write items that are below "WARN" in names(logging::loglevels)
+#' myLogger$debug("a debug statement")
+#' myLogger$info("a warn statement")
+#' 
+#' #Creating a logger with logLevel = "DEBUG"
+#' myLogger <- createLogger(logName = "com.mycompany.newprocessor", 
+#'                          logFileName = "newprocessor.log", 
+#'                          logLevel = "DEBUG")
+#' 
+#' #Now it will log debug and info statements
+#' myLogger$debug("a debug statement")
+#' myLogger$info("a warn statement")
+#' 
+logger <- function(bubble = TRUE, envir = parent.frame(), ...) {
+  if(!bubble) {
+    return(createLogger(envir = envir, ...))
+  } else {
+    allEnvironments <- as.list(sys.frames())
+    bubbleLoggerObj <- Filter( function(x) 'Logger' %in% class( get(x) ), ls(pattern = "bubbleLogger", envir = as.environment("package:racas")) )    
+    if(length(bubbleLoggerObj) > 0) {
+      allEnvironments <- as.list(c(sys.frames(),globalenv()))
+      loggerObject <- get("bubbleLogger", envir = as.environment("package:racas"))
+      if(any(unlist(lapply(allEnvironments, identical, loggerObject$envir)))) {
+        return(loggerObject)
+      }
+    }
+    unlockBinding( "bubbleLogger", as.environment("package:racas") ) 
+    assign("bubbleLogger", createLogger(envir = envir, ...), as.environment("package:racas"))
+    lockBinding( "bubbleLogger", as.environment("package:racas") ) 
+    return(get("bubbleLogger", envir = as.environment("package:racas")))
+  }
+  return()
+}
--- R/messenger.R
+++ R/messenger.R
@@ -1,50 +1,62 @@
-Messenger <- setRefClass("Messenger", 
-                       fields = list(errors = "character",
-                                     warnings = "character",
-                                     infos = "character"),
-                       methods = list(
-                         addError = function(x) {
-                           errors <<- c(errors,as.character(x))
-                         },
-                         setErrors = function(x) {
-                           errors <<- as.character(x)
-                         },
-                         addWarning = function(x) {
-                           warnings <<- c(warnings,as.character(x))
-                         },
-                         setWarnings = function(x) {
-                           warnings <<- as.character(x)
-                         },
-                         addInfo = function(x) {
-                           infos <<- c(infos,as.character(x))
-                         },
-                         setInfos = function(x) {
-                           infos <<- as.character(x)
-                         },
-                         reset = function() {
-                           errors <<- as.character()
-                           infos <<- as.character()
-                           warnings <<- as.character()
-                         },
-                         json = function() {
-                           return(toJSON(list(errors = errors, infos = infos, warnings = warnings)))
-                         }
-                       )
+Messenger <- setRefClass(Class = "Messenger", 
+                         fields = list(errors = "character",
+                                       warnings = "character",
+                                       infos = "character",
+                                       envir = "environment"),
+                         methods = list(
+                           addError = function(x) {
+                             errors <<- c(errors,as.character(x))
+                           },
+                           addWarning = function(x) {
+                             warnings <<- c(warnings,as.character(x))
+                           },
+                           addInfo = function(x) {
+                             infos <<- c(infos,as.character(x))
+                           },
+                           reset = function() {
+                             errors <<- as.character()
+                             infos <<- as.character()
+                             warnings <<- as.character()
+                             envir <<- parent.frame()
+                             return(.self)
+                           },
+                           captureOutput = function(expr, envir = parent.frame(), ...) {
+                             outputHandler <- new_output_handler(error = function(x) addError(x$message),
+                                                                 warning = function(x) addWarning(x$message),
+                                                                 message = function(x) addInfo(x$message),
+                             )
+                             evaledExpr <- evaluate(expr, envir = envir, output_handler = outputHandler)
+                           },
+                           toJSON = function() {
+                             return(rjson::toJSON(list("error" = length(errors)!=0,
+                                                       "warning" = length(warnings)!=0,
+                                                       info = length(infos)!=0,
+                                                       errors = errors, 
+                                                       warnings = warnings,
+                                                       infos = infos)))
+                           }
+                         )
 )
+setMethod ("toJSON", signature="Messenger", definition= function (x) x$toJSON() )
 
-bubbleMessenger <- Messenger$new()
-
-messenger <- function(bubble = TRUE) {
-  if(!bubble) {
-    return(Messenger$new())
+racasMessenger <- Messenger$new(envir = environment())
+messenger <- function(racas = TRUE, envir = parent.frame(), ...) {
+  if(!racas) {
+    return(Messenger$new(envir = envir, ...))
   } else {
-    bubbleMessengerObj <- Filter( function(x) 'Messenger' %in% class( get(x) ), ls(pattern = "bubbleMessenger", env = as.environment("package:racas")) )    
-    if(length(bubbleMessengerObj) > 0) {
-      return(get("bubbleMessenger", envir = as.environment("package:racas")))
-    } else {
-      assignInNamespace("bubbleMessenger", Messenger$new(),  ns="racas")
-      return(get("bubbleMessenger", envir = as.environment("package:racas")))
+    allEnvironments <- as.list(sys.frames())
+    racasMessengerObj <- Filter( function(x) 'Messenger' %in% class( get(x) ), ls(pattern = "racasMessenger", envir = as.environment("package:racas")) )    
+    if(length(racasMessengerObj) > 0) {
+      allEnvironments <- as.list(c(sys.frames(),globalenv()))
+      messengerObject <- get("racasMessenger", envir = as.environment("package:racas"))
+      if(any(unlist(lapply(allEnvironments, identical, messengerObject$envir)))) {
+        return(messengerObject)
+      }
     }
+    unlockBinding( "racasMessenger", as.environment("package:racas") ) 
+    assign("racasMessenger", Messenger$new(envir = envir, ...), as.environment("package:racas"))
+    lockBinding( "racasMessenger", as.environment("package:racas") ) 
+    return(get("racasMessenger", envir = as.environment("package:racas")))
   }
   return()
 }
