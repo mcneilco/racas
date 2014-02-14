33609ac6425be796900eaa23732ed6abc9f4ef42
Brian Bolt <brian@mcneilco.com>
2014-02-13 17:11:58 -0800

    adding ability to load conf file from relative path or racas install libpath

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,9 +1,10 @@
 .onLoad <- function(libname, pkgname) {
   appConfName <- paste0(applicationSettings$appName,"_CONFIG")
   appHomeName <- paste0(applicationSettings$appName,"_HOME")
+  relativeConf <- file.path(normalizePath(file.path("..","..")),"acas","conf","config.properties")
   appConf <- Sys.getenv(appConfName)
   appHome <- Sys.getenv(appHomeName)
-  if(appHome == ""  && appConf == "") {
+  if(appHome == ""  && appConf == "" && !file.exists(relativeConf)) {
     warning(paste0(appHomeName," and ",appConfName," environment variables not set, not reading application settings.\n\n",
                    "You can set this prior to loading this package by running:\n",
                    "\tSys.setenv(",appHomeName," = \"/path/to/",applicationSettings$appName,"\")\n",
@@ -14,11 +15,16 @@
     tryCatch({
       if(appHome!="") {
         configFileLocation <- file.path(appHome,"conf/compiled/conf.properties")
-        output <- readConfigFile(configFileLocation)
       } else {
-        configFileLocation <- file.path(appConf)
-        output <- readConfigFile(configFileLocation)
+        if(appConf!="") {
+          configFileLocation <- file.path(appConf)
+        } else {
+          configFileLocation <- relativeConf
+        }
       }
+      packageStartupMessage(paste("Using configuration file:",configFileLocation ))
+      output <- readConfigFile(configFileLocation)
+      
     },error = function(ex) {
       #error <- capture.output(print(ex))
       warning(paste0("Could not load configuration file at:\n\t",
