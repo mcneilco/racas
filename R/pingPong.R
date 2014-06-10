9df30cdad906cdfa42c3abbf720e1249795e01e1
Brian Bolt <brian@mcneilco.com>
2014-06-10 11:26:25 -0700

    ping pong table changes

--- R/pingPong.R
+++ R/pingPong.R
@@ -19,32 +19,32 @@
 #'   #indexes = lapply(list("AGV_ID", "AG_ID", "AG_PUBLIC_DATA", "AG_TESTED_LOT", "CV_ID", "C_ID", "C_PUBLIC_DATA", "C_STATE_ID", "PROJECT", "PROTOCOL_NAME", "SV_ID", "S_ID", "S_PUBLIC_DATA", "S_STATE_ID", "TGV_ID", "TG_ID", "TG_PUBLIC_DATA", "TG_STATE_ID", "TG_TESTED_LOT"), function(x) list(name = x, tableSpace = "KALYPSYSADMIN_NOLOG", options = "NOLOGGING", "compute statistics"))
 #'  indexes = lapply(list("AGV_ID"), function(x) list(name = x, tableSpace = "KALYPSYSADMIN_NOLOG", options = "NOLOGGING", "compute statistics"))
 #'  
-pingPong <- function(originView, intermediateTablePrefix = list(schema = racas::applicationSettings$server.database.username, name = originView$name, tableSpace = NULL, options = NULL), destinationViewName, primaryKey = NULL, indexes = NULL, applicationSettings = racas::applicationSettings) {
-  logger <- createLogger(logName = "com.mcneilco.acas.pingpongtables.generator")
+pingPong <- function(originView, intermediateTablePrefix = list(schema = racas::applicationSettings$server.database.username, name = originView$name, tableSpace = NA, options = c()), destinationViewName, primaryKey = NULL, indexes = NULL, applicationSettings = racas::applicationSettings) {
+  logger <- createLogger(logName = "com.mcneilco.racas.pingpong.apiviews")
   options(scipen=99)
   error_ping_pong_generator <- FALSE
   conn <- getDatabaseConnection(applicationSettings)
   on.exit(cat(dbDisconnect(conn)))
-  if (dbExistsTable(conn, name = paste0(intermediateTablePrefix$name,"_A"), schema = intermediateTablePrefix$schema)){
-    pingPongTableNew <- 'B'
-    pingPongTableOld <- 'A'
+  if (dbExistsTable(conn, name = paste0(intermediateTablePrefix$name,"_a"), schema = intermediateTablePrefix$schema)){
+    pingPongTableNew <- 'b'
+    pingPongTableOld <- 'a'
     logger$debug(paste0("New ",intermediateTablePrefix$name," table is ", pingPongTableNew))
     logger$debug(paste0("Old ",intermediateTablePrefix$name," table is ", pingPongTableOld))
   } else {
-    pingPongTableNew <- 'A'
-    pingPongTableOld <- 'B'
+    pingPongTableNew <- 'a'
+    pingPongTableOld <- 'b'
     logger$debug(paste0("New ",intermediateTablePrefix$name," table is ", pingPongTableNew))
     logger$debug(paste0("Old ",intermediateTablePrefix$name," table is ", pingPongTableOld))
   }
   
   #Check to see if both tables are preset A and B
-  if (dbExistsTable(conn, name = paste0(intermediateTablePrefix$name,"_A"), schema = intermediateTablePrefix$schema) & dbExistsTable(conn, name = paste0(intermediateTablePrefix$name,"_B"), schema = intermediateTablePrefix$schema)){
+  if (dbExistsTable(conn, name = paste0(intermediateTablePrefix$name,"_a"), schema = intermediateTablePrefix$schema) & dbExistsTable(conn, name = paste0(intermediateTablePrefix$name,"_b"), schema = intermediateTablePrefix$schema)){
     logger$warn(paste0(intermediateTablePrefix$schema,".",intermediateTablePrefix$name,' Table A and B are present for'))
   }
   
   #Create the intermediate table
   qu <- paste0("CREATE TABLE ",intermediateTablePrefix$schema,".",intermediateTablePrefix$name,"_", pingPongTableNew, " ",
-               ifelse(is.null(intermediateTablePrefix$tableSpace), "", paste("tablespace", intermediateTablePrefix$tableSpace)), " ",
+               ifelse(is.na(intermediateTablePrefix$tableSpace), "", paste("tablespace", intermediateTablePrefix$tableSpace)), " ",
                paste0(intermediateTablePrefix$options, collapse = " "),
                " AS SELECT * FROM ",originView$schema,".",originView$name)
   newResultTblCreated <- query(qu, applicationSettings = applicationSettings, conn = conn)
@@ -58,7 +58,7 @@ pingPong <- function(originView, intermediateTablePrefix = list(schema = racas::
   
   ##Primary Key creation
   if(!is.null(primaryKey)) {
-    qu <- paste0(" ALTER TABLE ",intermediateTablePrefix$schema,".",intermediateTablePrefix$name,"_", pingPongTableNew," ADD PRIMARY KEY (",primaryKey,") NOLOGGING ")
+    qu <- paste0(" ALTER TABLE ",intermediateTablePrefix$schema,".",intermediateTablePrefix$name,"_", pingPongTableNew," ADD PRIMARY KEY (",primaryKey,") ", paste0(intermediateTablePrefix$options, collapse = " "))
     primaryKeyCreated <- query(qu, applicationSettings = applicationSettings, conn = conn)
     if(class(primaryKeyCreated) == "list") {
       logger$error(qu)
@@ -78,7 +78,7 @@ pingPong <- function(originView, intermediateTablePrefix = list(schema = racas::
       idxName <- substr(idxName, nchar(idxName)-30+1, nchar(idxName))
       
       qu <- paste0(" CREATE INDEX ",idxName," ON ",intermediateTablePrefix$schema,".",intermediateTablePrefix$name,"_", pingPongTableNew," (",index$name,")  ",
-                   ifelse(is.null(index$tableSpace), "", paste0("tablespace ", index$tableSpace)), " ",
+                   ifelse(is.na(index$tableSpace), "", paste0("tablespace ", index$tableSpace)), " ",
                    paste0(index$options, collapse = " "))
       idxCreated <- query(qu, applicationSettings = applicationSettings, conn = conn)
       if(class(idxCreated) == "list") {
@@ -91,7 +91,7 @@ pingPong <- function(originView, intermediateTablePrefix = list(schema = racas::
     }
   }
   ## refresh views
-  qu <- paste0(" CREATE OR REPLACE FORCE VIEW ",destinationViewName$schema,".",destinationViewName$name,"
+  qu <- paste0(" CREATE OR REPLACE VIEW ",destinationViewName$schema,".",destinationViewName$name,"
                as
                SELECT *
                FROM ",intermediateTablePrefix$schema, ".", intermediateTablePrefix$name,"_", pingPongTableNew,"
@@ -115,11 +115,9 @@ pingPong <- function(originView, intermediateTablePrefix = list(schema = racas::
   }
   
   if(error_ping_pong_generator) {
-    rollback <- dbRollback(conn)
     logger$error("PING-PONG tables update unsuccessful, rolledback ")
     stop(paste0("PING-PONG tables update unsuccessful, rolled back\n for details see\npingpongtables.log"))
   } else {
-    commited <- dbCommit(conn)
     logger$info("PING-PONG tables successfully updated and committed")
   }
 }
