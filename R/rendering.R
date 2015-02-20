7b933d0cb14efeb90393550e450eefd78f6fdadc
Brian Bolt <brian@mcneilco.com>
2015-02-19 18:35:59 -0800

    get rendering parameters passed in from rapache

--- R/rendering.R
+++ R/rendering.R
@@ -704,3 +704,102 @@ modify_or_remove_zero_dose_points <- function(points, logDose) {
     .N)]
   return(points[dose!=0,])
 }
+
+get_rendering_hint_options <- function(renderingHint) {
+  renderingOptions <- switch(renderingHint,
+                             "4 parameter D-R" = list(fct = LL4, paramNames = c("ec50", "min", "max", "slope"), drawIntercept = "ec50"),
+                             "Ki Fit" = list(fct = OneSiteKi, paramNames = c("ki", "min", "max", "kd", "ligandConc"),drawIntercept = "ki" ),
+  )
+  return(renderingOptions)
+}
+
+parse_params_curve_render_dr <- function(getParams = GET) {
+  # Get data
+  if(is.null(getParams$ymin)) {
+    yMin <- NA
+  } else {
+    yMin <- as.numeric(getParams$ymin)
+  }
+  if(!is.null(getParams$yNormMin)) {
+    yMin <- as.numeric(getParams$yNormMin)
+  }
+  if(is.null(getParams$ymax)) {
+    yMax <- NA
+  } else {
+    yMax <- as.numeric(getParams$ymax)
+  }
+  if(!is.null(getParams$yNormMax)) {
+    yMax <- as.numeric(getParams$yNormMax)
+  }
+  if(is.null(getParams$xmin)) {
+    xMin <- NA
+  } else {
+    xMin <- as.numeric(getParams$xmin)
+  }
+  if(!is.null(getParams$xNormMin)) {
+    xMin <- as.numeric(getParams$xNormMin)
+  }
+  if(is.null(getParams$xmax)) {
+    xMax <- NA
+  } else {
+    xMax <- as.numeric(getParams$xmax)
+  }
+  if(!is.null(getParams$xNormMax)) {
+    xMax <- as.numeric(getParams$xNormMax)
+  }
+  if(is.null(getParams$height)) {
+    height <- 500
+  } else {
+    height <- as.numeric(getParams$height)
+  }
+  if(!is.null(getParams$cellHeight)) {
+    height <- as.numeric(getParams$cellHeight)
+  }
+  if(is.null(getParams$width)) {
+    width <- 700
+  } else {
+    width <- as.numeric(getParams$width)
+  }
+  if(!is.null(getParams$cellWidth)) {
+    width <- as.numeric(getParams$cellWidth)
+  }
+  if(is.null(getParams$inTable)) {
+    inTable <- FALSE
+  } else {
+    inTable <- as.logical(getParams$inTable)
+  }
+  if(is.null(getParams$showAxes)) {
+    showAxes <- TRUE
+  } else {
+    showAxes <- as.logical(getParams$showAxes)
+  }
+  if(is.null(getParams$showGrid)) {
+    showGrid <- !inTable
+  } else {
+    showGrid <- as.logical(getParams$showGrid)
+  }
+  if(is.null(getParams$labelAxes)) {
+    labelAxes <- !inTable
+  } else {
+    labelAxes <- as.logical(getParams$labelAxes)
+  }
+  if(is.null(getParams$legend)) {
+    legend <- !inTable
+  } else {
+    legend <- as.logical(getParams$legend)
+  }
+  
+  if(is.null(getParams$curveIds)) {
+    stop("curveIds not provided, provide curveIds")
+    DONE
+  } else {
+    curveIds <- getParams$curveIds
+    curveIdsStrings <- strsplit(curveIds,",")[[1]]
+    curveIds <- suppressWarnings(as.integer(curveIds))
+    if(is.na(curveIds)) {
+      curveIds <- curveIdsStrings
+    }
+  }
+  
+  return(list(yMin = yMin, yMax = yMax, xMin = xMin, xMax = xMax, height = height, width = width, inTable = inTable, showAxes = showAxes, labelAxes = labelAxes, showGrid = showGrid, legend = legend, curveIds = curveIds))
+}
