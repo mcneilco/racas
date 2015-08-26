4928cea16554afa93a707b70fb6c51d34d061066
Brian Bolt <brian@mcneilco.com>
2015-08-25 17:47:37 -0700

    fix issue with NA operators in data frame being sent to GUI

--- R/dose_response_api.R
+++ R/dose_response_api.R
@@ -366,7 +366,7 @@ get_curve_attributes.LL4 <- function(fitData, saved = TRUE) {
   if(saved) {
     return(list(
       EC50 = fitData$ec50[[1]],
-      Operator = fitData$ec50OperatorKind,
+      Operator = na_to_null(fitData$ec50OperatorKind),
       SST =  fitData$sst[[1]],
       SSE =  fitData$sse[[1]],
       rsquare = fitData$rsquared[[1]],
@@ -392,7 +392,7 @@ get_curve_attributes.ki <- function(fitData, saved = TRUE) {
   if(saved) {
     return(list(
       Ki = fitData$ki[[1]],
-      Operator = fitData$kiOperatorKind,
+      Operator = na_to_null(fitData$kiOperatorKind),
       SST =  fitData$sst[[1]],
       SSE =  fitData$sse[[1]],
       rsquare = fitData$rsquared[[1]],
