d352f4339110589397f204abfb4e2cc9c3f6b20e
Brian Bolt <brian@mcneilco.com>
2016-01-26 16:34:12 -0800

    allow the rsource to provide rendering hint options

--- R/rendering.R
+++ R/rendering.R
@@ -656,6 +656,15 @@ get_rendering_hint_options <- function(renderingHint = NA) {
   renderingOptions <- switch(renderingHint,
                              "4 parameter D-R" = list(fct = LL4, paramNames = c("ec50", "min", "max", "slope"), drawIntercept = "ec50"),
                              "Ki Fit" = list(fct = OneSiteKi, paramNames = c("ki", "min", "max", "kd", "ligandConc"),drawIntercept = "ki" ),
+                             {
+                               modelFitClasses <- rbindlist(fromJSON(applicationSettings$client.curvefit.modelfitparameter.classes))
+                               source(file.path(applicationSettings$appHome,modelFitClasses[code==renderingHint]$RSource), local = TRUE)
+                               if(exists('renderingOptions')) {
+                                 renderingOptions
+                               } else {
+                                 stop("no rendering hint options found")
+                               }
+                             }
   )
   return(renderingOptions)
 }
